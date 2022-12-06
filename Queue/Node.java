5f234512c10a78e52371bbaa44b9c265b6c6207d
Majid Saqr <104238392+majidsaqr12@users.noreply.github.com>
2022-12-06 21:10:00 +0200

    Update Node.java

--- Queue/Node.java
+++ Queue/Node.java
@@ -10,4 +10,13 @@ package com.mycompany.queue;
  */
 public class Node {
     
+    public int data;
+    public Node next;
+    
+    public Node()
+    {
+        data = 0;
+        next = null;
+    }
+    
 }
